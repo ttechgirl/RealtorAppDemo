b1afff8ce0f11dc593c496a1cdbfc5c2691ee03e
ttechgirl <abiodunayisat2@gmail.com>
2023-04-20 21:56:00 +0100

    create the header componets and its navigation

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import LogIn from "./pages/LogIn";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header/>
+        
         <Routes>
           <Route path="/" element={<Home/>}> </Route>
           <Route path="/forgot-password" element={<ForgotPassword/>}> </Route>
---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+//the website header
+import React from 'react'
+//referencing location and navigate the path or route
+import { useLocation, useNavigate} from 'react-router'
+
+export default function Header() {
+    const location = useLocation()
+    const navigate= useNavigate()
+
+    //console.log(location.pathname);
+    function pathRoute(route){
+        if(route == location.pathname){
+            return true;
+        }
+    }
+  return (
+    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
+        <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+            <div className=''>
+                {/*logo*/}
+                <img src='https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg' alt='logo' className='h-5 cursor-pointer' onClick={()=>navigate('/')}/>
+            </div>
+            <div>
+                {/*header menu list*/}
+                <ul className='flex space-x-10 cursor-pointer'>
+                    <li className={`py-3 text-sm  font-semibold border-b-[3px] text-gray-400 border-b-transparent ${pathRoute('/') && 'text-black border-b-red-900'}`} onClick={()=>navigate('/')} >Home</li>
+                    <li className={`py-3 text-sm  font-semibold border-b-[3px] text-gray-400 border-b-transparent ${pathRoute('/offers') && 'text-black border-b-red-900'}`} onClick={()=>navigate('/offers')}>Offers</li>
+                    <li className={`py-3 text-sm  font-semibold border-b-[3px] text-gray-400 border-b-transparent ${pathRoute('/sign-in') && 'text-black border-b-red-900'}`} onClick={()=>navigate('/sign-in')}>Sign In</li>
+                </ul>
+            </div>
+        </header>
+    </div>
+  )
+}
